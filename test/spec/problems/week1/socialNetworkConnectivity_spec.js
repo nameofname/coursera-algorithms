267316c216bc191e942b1c7d30153b2b57d80dd2
nameofname <nameofemail@gmail.com>
2017-05-14 18:22:39 -0400

    more tests and started adding in cononical solution

--- test/spec/problems/week1/socialNetworkConnectivity_spec.js
+++ test/spec/problems/week1/socialNetworkConnectivity_spec.js
@@ -15,6 +15,20 @@ describe('Social Network Connectivity', () => {
         expect(socialNetworkConnectivity1.largest).to.equal(5);
     });
 
+    it('should should return the size of the set if every element in the set is conected', () => {
+        const sn = new SocialNetworkConnectivity(10);
+        sn.union(0, 1);
+        sn.union(1, 2);
+        sn.union(2, 3);
+        sn.union(3, 4);
+        sn.union(4, 5);
+        sn.union(5, 6);
+        sn.union(6, 7);
+        sn.union(7, 8);
+        sn.union(8, 9);
+        expect(sn.largest).to.equal(10)
+    });
+
     it('should help you calculate when an entire starting set is connected', () => {
         const n = 100;
         let i = 0;
@@ -26,6 +40,5 @@ describe('Social Network Connectivity', () => {
         }
         expect(i).to.equal(106);
     });
-
 
 });
